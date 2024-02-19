68231a6bf732569d80900161916bca2d2ba5538b
ace01603 <50892754+ace01603@users.noreply.github.com>
2024-02-19 07:37:52 +0000

    W3L2

---
+++ Lectures/W3L2 switch statements/basicSwitch/sketch.js
@@ -0,0 +1,18 @@
+/**
+ * Try changing the value of input to see how the switch works.
+ * Try removing one or more of the break statements;
+ */
+let input = 2;
+switch (input) {
+    case 1:
+        console.log("This is the first case");
+        break;
+    case 2:
+        console.log("This is the second case");
+        break;
+    case 3:
+        console.log("This is the third case");
+        break;
+    default:
+        console.log("This is the default");
+}
