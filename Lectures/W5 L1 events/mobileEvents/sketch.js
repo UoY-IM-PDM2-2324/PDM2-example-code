fe624af035a880a307bb37546d6635d101d720c3
ace01603 <50892754+ace01603@users.noreply.github.com>
2024-02-26 16:00:14 +0000

    added mobile events

--- Lectures/W5 L1 events/mobileEvents/sketch.js
+++ Lectures/W5 L1 events/mobileEvents/sketch.js
@@ -1,8 +1,37 @@
+let shapeFunctions;
+let currentIndex = 0;
+let fillColour;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    shapeFunctions = [circle, square]
+    fillColour = getRandomColour();
 }
 
 function draw() {
     background(0);
-    circle(width / 2, height / 2, 50);
+    fill(fillColour);
+    shapeFunctions[currentIndex](width / 2, height / 2, 50);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function deviceMoved() {
+    fillColour = getRandomColour();
+}
+
+function deviceTurned() {
+    // Change shape
+    currentIndex = (currentIndex + 1) % shapeFunctions.length;
+}
+
+
+/**
+ * Gets a random colour
+ * @returns {color}
+ */
+function getRandomColour() {
+    return color(random(255), random(255), random(255));
 }
