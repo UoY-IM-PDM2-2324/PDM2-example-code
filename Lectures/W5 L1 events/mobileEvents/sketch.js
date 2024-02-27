eda017b0f75818ba16c2f7ac62184fd4d0e60121
ace01603 <50892754+ace01603@users.noreply.github.com>
2024-02-27 06:19:36 +0000

    update basic touch events

--- Lectures/W5 L1 events/mobileEvents/sketch.js
+++ Lectures/W5 L1 events/mobileEvents/sketch.js
@@ -1,19 +1,30 @@
+const colours = new Map();
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
+    for (let i = 1; i <= 5; i++) {
+        colours.set(i, getRandomColour());
+    }
 }
 
 function draw() {
-    for (const t of touches) {
-        circle(t.x, t.y, 50);
-    }
-    if (touches.length === 5) {
+    if (touches.length === 0 || touches.length > 5) {
         background(0); // clear the canvas
+    }
+    else {
+        fill(colours.get(touches.length));
+        for (const t of touches) {
+            circle(t.x, t.y, 50);
+        }
     }
 }
 
+/**
+ * p5.js event function that fires when the window changes size
+ */
 function windowResized() {
+    // p5.js function to resize the canvas
     resizeCanvas(windowWidth, windowHeight);
 }
 
