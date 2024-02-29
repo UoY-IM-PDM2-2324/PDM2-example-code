e808cf84e74457cea42dbc12c6757fae16a300b1
ace01603 <50892754+ace01603@users.noreply.github.com>
2024-02-29 16:07:43 +0000

    More W5L2 examples

--- Lectures/W5L2 custom GUI controls/pieMenu/sketch.js
+++ Lectures/W5L2 custom GUI controls/pieMenu/sketch.js
@@ -1,14 +1,22 @@
 let inventoryMenu;
-
+const itemImages = new Map();
+const placedItems = [];
 
 function setup() {
     createCanvas(600, 400);
+    getItemImages();
     const menuOptions = createMenuOptions();
     inventoryMenu = new PieMenu(menuOptions);
 }
 
 function draw() {
     background(0);
+    for (const item of placedItems) {
+        // Check that the item image has been loaded
+        if (itemImages.has(item.name)) {
+            image(itemImages.get(item.name), item.x, item.y, 128, 128)
+        }
+    }
     inventoryMenu.draw();
 }
 
@@ -27,7 +35,7 @@ function createMenuOptions() {
     options.push(new MenuButton("assets/apple.png", color(99, 99, 255), () => menuOptionClicked("apple")));
     options.push(new MenuButton("assets/axe.png", color(99, 99, 255), () => menuOptionClicked("axe")));
     options.push(new MenuButton("assets/book.png", color(99, 99, 255), () => menuOptionClicked("book")));
-    options.push(new MenuButton("assets/camera.png", color(99, 99, 255), () => menuOptionClicked("camera")));
+    options.push(new MenuButton("assets/camera.png", color(99, 99, 255), () => menuOptionClicked("photo")));
     options.push(new MenuButton("assets/orange.png", color(99, 99, 255), () => menuOptionClicked("orange")));
     options.push(new MenuButton("assets/pear.png", color(99, 99, 255), () => menuOptionClicked("pear")));
     options.push(new MenuButton("assets/wallpaper.png", color(99, 99, 255), () => menuOptionClicked("wallpaper")));
@@ -36,8 +44,40 @@ function createMenuOptions() {
 
 /**
  * Click event handler for all menu buttons.
- * @param {string} msg The message to print.
+ * @param {string} item The name of the item to show.
  */
-function menuOptionClicked(msg) {
-    console.log(msg);
+function menuOptionClicked(item) {
+    placedItems.push({
+        name: item,
+        x: inventoryMenu.getX(),
+        y: inventoryMenu.getY()
+    })
+}
+
+
+/**
+ * Fill the map of item images. Images will be loaded asynchronously and added 
+ * to the map when they are ready.
+ */
+function getItemImages() {
+    /**
+     * The second argument in each loadImage() below is an anonymous callback 
+     * function written using arrow syntax. Each function is the following 
+     * traditional anomyous function:
+     * 
+     * function (img) {
+     *   itemImages.set("name", img);
+     * }
+     * 
+     * The argument, img, is automatically passed to the callback function by 
+     * the loadImage function
+     */
+    loadImage("assets/Apple_Tree_NH_Icon.png", img => itemImages.set("apple", img));
+    loadImage("assets/orange_128.png", img => itemImages.set("orange", img));
+    loadImage("assets/Pear_Tree_NH_Icon.png", img => itemImages.set("pear", img));
+    loadImage("assets/Framed_Photo_(Natural_-_Pressed_Flower)_NH_Icon.png", img => itemImages.set("photo", img));
+    loadImage("assets/Book_(Western_Literature)_NH_Icon.png", img => itemImages.set("book", img));
+    loadImage("assets/Wallpaper_NH_Inv_Icon.png", img => itemImages.set("wallpaper", img));
+    loadImage("assets/1280px-Axe_PG_Model.png", img => itemImages.set("axe", img));
+
 }
